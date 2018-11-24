fc6e9fb975e7c8869e07a9a2b52e5948d3733f8a
mattp103 <41234097+mattp103@users.noreply.github.com>
2018-11-24 17:27:42 +0000

    login and register

--- etcc/settings.py
+++ etcc/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'user',
+    'br',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = '/static/'
--- user/views.py
+++ user/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.hashers import check_password
+from django.contrib import messages
 
-# Create your views here.
+def signin(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        user = User.objects.get(email=email)
+
+        if user.check_password(password):
+            login(request, user)
+
+    return render(request, 'user/login.html')
+
+def signup(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        password1 = request.POST.get('password1')
+        password2 = request.POST.get('password2')
+
+        if len(name.split(' ')) == 1:
+            first_name = name.split(' ')[0]
+            user = User.objects.create_user(username=first_name, first_name=first_name, email=email, password=password1)
+
+        elif len(name.split(' ')) > 1:
+            first_name = name.split(' ')[0]
+            last_name = name.split(' ')[len(name.split(' '))-1]
+
+            user = User.objects.create_user(username=first_name, first_name=first_name, last_name=last_name, email=email, password=password1)
+
+
+        login(request, user)
+
+    return render(request, 'user/register4.html')
