d04cba4b7bee2daba280a7e0cae856b325a8a746
Ethan <ethan@thehowards.me.uk>
2019-01-13 15:58:09 +0000

    coumight

--- br/admin.py
+++ br/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Comment, Reading, Progress
+from .models import *
 
 admin.site.register(Comment)
 admin.site.register(Reading)
--- br/models.py
+++ br/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 import datetime
 from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class Notice(models.Model):
     title = models.CharField(max_length=100)
